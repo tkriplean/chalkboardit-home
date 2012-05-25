f34f7bf9ff9072c769a8e84c64cb5ecb38eddcf1
Travis Kriplean <travis@cs.washington.edu>
2012-05-25 16:18:13 -0700

    enabling old homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+  def old
+    render :layout => 'old'
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ChalkboardIt::Application.routes.draw do
   root :to => "home#index"
   resources :accounts
+
+  match 'old', :to => "home#old"
 end
